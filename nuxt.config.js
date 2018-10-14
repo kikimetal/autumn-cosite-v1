df23253dae3ea25136a08b968f7f1a42a31b00de
kikimetal <reezafratzitz2@gmail.com>
2018-10-14 22:02:58 +0900

    nuxtスターター + cvl-cosite-v2 からのモジュールコピペ

---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  mode: 'spa',
+
+  head: {
+    title: 'autumn-cosite-v1',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
+      { hid: 'description', name: 'description', content: 'autumn corporate site' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+    ],
+    link: [
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // { rel: 'stylesheet', href: 'https://use.typekit.net/ozu0idc.css' },
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    * Nuxt ^2.0.0 build error 回避のため
+    * https://qiita.com/m1ul24/items/efd2b9af1954cce6bcc7
+    */
+    // extend (config, { isDev, isClient }) {
+    extend (config) {
+      // if (isDev && isClient) {
+      if (process.server && process.browser) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+  },
+
+  plugins: [{ src: '~/plugins/routerOption.js', ssr: false }],
+
+  css: [
+    'assets/css/myreset.scss',
+    'assets/css/animate.css',
+    'assets/css/common.scss',
+  ],
+}
---
+++ store/index.js
@@ -0,0 +1,36 @@
+import Vuex from 'vuex'
+// import axios from 'axios'
+import heads from '~/assets/heads.js'
+import ScrollOut from "scroll-out"
+
+const store = () => new Vuex.Store({
+
+  state: {
+    isScrollTop: true,
+    isShowNav: false,
+    getHead: () => heads[$nuxt.$route.path],
+    so: () => ScrollOut({
+      cssProps: {
+        // viewportY: true,
+        visibleY: true
+      }
+    })
+  },
+
+  mutations: {
+    setIsScrollTop (state) {
+      const isScrollTop = window.scrollY < 100
+      if (state.isScrollTop === isScrollTop) return
+      state.isScrollTop = isScrollTop
+    },
+    toggleNav (state, isShow) {
+      if (isShow === undefined) {
+        state.isShowNav = !state.isShowNav
+      }else{
+        state.isShowNav = isShow
+      }
+    },
+  }
+})
+
+export default store
